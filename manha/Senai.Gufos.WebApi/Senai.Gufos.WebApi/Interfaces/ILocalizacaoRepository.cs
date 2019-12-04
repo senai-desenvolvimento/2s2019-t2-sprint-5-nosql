d741365416dca67cf4096cb6f14748f9bc753b76
Helena Strada <helena.strada@info.local>
2019-12-04 09:57:58 -0200

    up

---
+++ manha/Senai.Gufos.WebApi/Senai.Gufos.WebApi/Interfaces/ILocalizacaoRepository.cs
@@ -0,0 +1,12 @@
+﻿using Senai.Gufos.WebApi.Domains;
+using System.Collections.Generic;
+
+namespace Senai.Gufos.WebApi.Interfaces
+{
+    public interface ILocalizacaoRepository
+    {
+        void Cadastrar(Localizacoes localizacoes);
+
+        List<Localizacoes> Listar();
+    }
+}
